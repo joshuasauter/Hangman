1456152f97c11c12cb3f4f94e0f0b188653c49a8
roramigator <roramigator@gmail.com>
2019-10-23 19:30:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/*
+*	Required libraries.
+*/
+
+const prompt = require("readline-sync");
+const wordBank = require("./word-bank.json");
+
+const welcomeFlag = "\nWelcome to Hangman!\nPress Ctrl+C to stop\n";
+
+function drawHangman(){
+	//const guiMan = "\n╔═════════════╗\n║             ║\n║             ║\n║             ║\n║             ║\n╚═════════════╝\n";
+	const guiMan = '😄😊😐😞😱😵';
+	return guiMan;
+}
+
+while(true){
+	console.log(drawHangman());
+	//const letter = prompt.question("Please guess a letter: ");
+	break;
+}
